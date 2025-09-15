9bb352ccae1ae0876adc9c4602e6990e31cd1e15
Eduard <nikinna86@gmail.com>
2025-09-15 19:00:31 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react-swc";
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     sourcemap: true,
-//   },
-// });
-
-// import { defineConfig } from "vite";
-
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/blended-react-firstmodule/",
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
 });
